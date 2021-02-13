06b5f6aaab89a8446e429cadac3ebb36c55446cb
artem <44943333+artVG@users.noreply.github.com>
2021-02-14 00:57:26 +0300

    Add project files.

---
+++ Creator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace randstr
+{
+    class Creator
+    {
+        private int minStringLen;
+        private int maxStringLen;
+        private int stringsNumber;
+        private string charPool;
+        private Random random;
+        private string[] resultStrings;
+        public string[] ResultStrings
+        {
+            get
+            {
+                return resultStrings;
+            }
+        }
+        
+
+        public Creator(int minLen, int maxLen, int number, string pool, int seed)
+        {
+            minStringLen = minLen;
+            maxStringLen = maxLen;
+            stringsNumber = number;
+            charPool = pool;
+            random = new Random(seed);
+        }
+
+        public Creator(int minLen, int maxLen, int number, string pool)
+        {
+            minStringLen = minLen;
+            maxStringLen = maxLen;
+            stringsNumber = number;
+            charPool = pool;
+            random = new Random();
+        }
+
+        public void Create()
+        {
+            resultStrings = new string[stringsNumber];
+            for (int stringIt = 0; stringIt < stringsNumber; stringIt++)
+            {
+
+                resultStrings[stringIt] = new string(CreateRandomArray());
+            }
+        }
+
+        private char[] CreateRandomArray()
+        {
+            int randomSringLen = random.Next(minStringLen, maxStringLen);
+            char[] randomChars = new char[randomSringLen];
+            for (int charIt = 0; charIt < randomSringLen; charIt++)
+            {
+                int randCharInPool = random.Next(charPool.Length-1);
+                randomChars[charIt] = charPool[randCharInPool];
+            }
+            return randomChars;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace randstr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Creator creator = ParseCreator(args);
+                creator.Create();
+                string outputFile = ParseOutFile(args);
+                Write(outputFile, creator.ResultStrings);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+
+        private static void Write(string outputFile, string[] resultStrings)
+        {
+            // Create the file, or overwrite if the file exists.
+            using (System.IO.FileStream fs = System.IO.File.Create(outputFile))
+            {
+                foreach (string resultString in resultStrings)
+                {
+                    byte[] info = new System.Text.UTF8Encoding(true).GetBytes(resultString + '\n');
+                    // Add information to the file.
+                    int offset = 0;
+                    fs.Write(info, offset, info.Length);
+                    offset += info.Length;
+                }
+
+            }
+        }
+
+        private static string WrongInput()
+        {
+            return "Please enter (not null) a arguments: output file name, min length, max length, number of strings, characters pool. Seed(optional)";
+        }
+
+        private static Creator ParseCreator(string[] args)
+        {
+            if (args.Length < 5)
+            {
+                throw new ArgumentException(WrongInput());
+            }
+            else
+            {
+                int minStringLen;
+                int maxStringLen;
+                int stringNumber;
+                string charPool = args[4];
+                int.TryParse(args[1], out minStringLen);
+                int.TryParse(args[2], out maxStringLen);
+                int.TryParse(args[3], out stringNumber);
+                int charPoolLen = charPool.Length - 1;
+                if (minStringLen > 0 &&
+                    maxStringLen > 0 &&
+                    stringNumber > 0 &&
+                    charPoolLen > 0
+                    )
+                {
+                    if (args.Length < 6)
+                    {
+                        return new Creator(minStringLen, maxStringLen, stringNumber, charPool);
+                    }
+                    else
+                    {
+                        int seed;
+                        if (int.TryParse(args[5], out seed))
+                        {
+                            return new Creator(minStringLen, maxStringLen, stringNumber, charPool, seed);
+                        }
+                        else
+                        {
+                            throw new ArgumentException(WrongInput());
+                        }
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException(WrongInput());
+                }
+            }
+        }
+
+        private static string ParseOutFile(string[] args)
+        {
+            string outputFile = args[0];
+            if (outputFile.Length > 0)
+            {
+                return outputFile;
+            }
+            else
+            {
+                throw new ArgumentException(WrongInput());
+            }
+        }
+    }
+}
